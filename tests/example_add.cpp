7337e24dad088af485e439eb6da25d0bd67bf2b3
Danilin <danilin.alexandr@inbox.ru>
2023-06-06 13:57:09 +0300

    working google tests

--- tests/example_add.cpp
+++ tests/example_add.cpp
@@ -1,9 +1,6 @@
 #include <gtest/gtest.h>
 
-#include "example.h"
-
 TEST(example, add) {
-  double res;
-  res = add_numbers(1.0, 2.0);
-  ASSERT_NEAR(res, 3.0, 1.0e-11);
+	EXPECT_EQ(7 * 6, 42);
+    EXPECT_EQ(7 * 5, 42);
 }
