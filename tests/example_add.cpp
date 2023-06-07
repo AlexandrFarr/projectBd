cc0f154d91809f0a45cc4f5d737c760599a287cc
Danilin <danilin.alexandr@inbox.ru>
2023-06-07 16:59:28 +0300

    f

--- tests/example_add.cpp
+++ tests/example_add.cpp
@@ -1,9 +1,13 @@
 #include <gtest/gtest.h>
+//#include "example.h"
 
 TEST(example, add) {
+   // double res;
+   //res = add_numbers(1.0, 2.0);
+
 	EXPECT_EQ(7 * 6, 42);
-    EXPECT_EQ(7 * 5, 42);
-    exit(1);
+    //EXPECT_EQ(res, 42);
+
 }
 
 
