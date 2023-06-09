ca1526c3bbc2446789b2104163e537cfc3a67b68
Danilin <danilin.alexandr@inbox.ru>
2023-06-09 15:09:09 +0300

    add test for 1 example

--- tests/example_add.cpp
+++ tests/example_add.cpp
@@ -1,12 +1,15 @@
 #include <gtest/gtest.h>
-//#include "example.h"
+//#include "../src/example.h"
+//#include "../src/example.h"
+#include <../src/example.h>
 
-TEST(example, add) {
-   // double res;
-   //res = add_numbers(1.0, 2.0);
+
+TEST(example, add_numbers) {
+    double res;
+    res = add_numbers(1.0, 2.0);
 
 	EXPECT_EQ(7 * 6, 42);
-    //EXPECT_EQ(res, 42);
+    EXPECT_EQ(res, 42);
 
 }
 
