30b3b8aa625331837fca572fbb2dcca85baabb19
Danilin <danilin.alexandr@inbox.ru>
2023-06-23 15:08:21 +0300

    work with tests

--- tests/example_add.cpp
+++ tests/example_add.cpp
@@ -6,15 +6,30 @@
 #include <QtSql>
 #include <QSqlDatabase>
 #include <QSqlTableModel>
+#include <QVector>
 
 
 
 TEST(example, add_numbers) {
 
+    //Arrange
     connectPost *bd = new connectPost();
+
     bd->enterQueryButton();
+    QVector<QString> vec (6,"");
+    //valid data
+    vec[0] = "QPSQL";
+    vec[1] = "127.0.0.1";
+    vec[2] = "testdb";
+    vec[3] = "postgres";
+    vec[4] = "users1";
+    vec[5] = "skansict272";
+
+    //Act
+    bd->creatConnect(vec);
 
 
+    //ASSERT
 	EXPECT_EQ(7 * 6, 42);
 
 
